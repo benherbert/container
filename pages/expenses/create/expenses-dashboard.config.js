937edfc781f42aec845e6c17bbd651c1f174767b
Ben Herbert <ben.herbert@crunch.co.uk>
2018-11-27 09:45:36 +0000

    BH copying files over to force folders up

---
+++ pages/expenses/create/expenses-dashboard.config.js
@@ -0,0 +1,16 @@
+export const pageConfig__expensesDashboard = {
+  meta: {
+    title: 'Crunch - Expenses Dashboard',
+    description: ''
+  },
+  presentation: {
+    pageTitle: 'Your Expenses Dashboard',
+  },
+  route: {
+    id: '1010',
+    name: 'expenses-dashboard',
+    path: '/expenses/dashboard',
+  }
+}
+
+
